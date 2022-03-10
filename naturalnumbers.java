91f8431757aaaae0d84c11d300bf978e17b8e3d2
RONALDO MATTHEW J R <95460329+ronaldo0820@users.noreply.github.com>
2022-03-10 12:09:09 +0530

    Create naturalnumbers.java

---
+++ naturalnumbers.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Main{
+ 
+    
+  public static void main(String[] args)
+    {     
+    int i;
+	System.out.print("The first 50 natural numbers are:\n");
+	for (i=1;i<=50;i++)
+	{      
+    	System.out.print(i+" ");
+	}
+      
+    }
+}
