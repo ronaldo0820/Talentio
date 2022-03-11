bb4ab4d66232bdad372725b442b2413252aa3f41
RONALDO MATTHEW J R <95460329+ronaldo0820@users.noreply.github.com>
2022-03-11 10:11:22 +0530

    Create 3Darray.java

---
+++ 3Darray.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+            int[][][] arr = {
+         {
+            {1, 2, 3, 4},{5, 6, 7, 8},{9, 10, 11, 12}
+         },
+         {
+            {13, 14, 15, 16},{17, 18, 19, 20},{21, 22, 23, 24}
+         }};
+      
+      for(int i=0; i<2; i++)
+      {
+         for(int j=0; j<3; j++)
+         {
+            for(int k=0; k<4; k++)
+            {
+               System.out.print("arr["+i+"]["+j+"]["+k+"] = " +arr[i][j][k]+"\t");
+            }
+            System.out.print("\n");
+         }
+      }
+
+   }
+}
